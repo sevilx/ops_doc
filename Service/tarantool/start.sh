61462ed8dc2d193b157da2cd5f9c0883c9fc2872
liquanzhou <quanzhou722@163.com>
2020-03-06 18:40:59 +0800

    up

--- Service/tarantool/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#pid=`cat box.pid`
-#kill -9 $pid
-#tarantool_box -c tarantool.cfg --init-storage
-tarantool_box -c tarantool.cfg --background
-
--- Service/tarantool/stop.sh
+++
@@ -1,2 +0,0 @@
-pid=`cat box.pid`
-kill -9 $pid
